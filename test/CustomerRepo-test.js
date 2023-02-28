8976e4a1e2621233764ec0936258c81da6214a65
Jesus Borjas <jesusbor006@gmail.com>
2023-02-28 16:09:07 -0600

    Add CustomerRepo class with test file

---
+++ test/CustomerRepo-test.js
@@ -0,0 +1,26 @@
+import chai from "chai";
+const expect = chai.expect;
+import CustomerRepo from "../src/classes/CustomerRepo";
+import customersTestData from "../src/data/customerTestData";
+
+describe("CustomerRepo", () => {
+  let customerRepo;
+  let customerData;
+
+  beforeEach(() => {
+    customerData = customersTestData;
+    customerRepo = new CustomerRepo(customerData)
+  });
+
+  it("Should be a function", () => {
+    expect(CustomerRepo).to.be.a("function");
+  });
+
+  it("Should be an instance of Customer Repo", () => {
+    expect(customerRepo).to.be.an.instanceOf(CustomerRepo)
+  })
+
+  it("Should have a property of customers", () => {
+    expect(customerRepo.customers).to.deep.equal(customerData);
+  });
+});
